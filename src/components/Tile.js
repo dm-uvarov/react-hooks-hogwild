4b68a5118dd4fa3f54dd8c5a46ec1f6b2148ff30
Jimmy <dm.a.uvarov@gmail.com>
2022-02-04 11:04:51 -0500

    made some changes

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Tile({name, image}){
+
+function Tile({name, image, id}){
     // console.log({name, image})
+
+    const [hogData,setHogData] = useState([]);
+
+    const handleOnClick = () => {
+        
+    }
+
+
+
+
     return (
-        <div className='ui eight wide column'>
-            <h5 key={name}> {name} </h5> 
-            <img key={image} src={image} alt='Hog'/> 
+        <div key = {id+name+"container"} className='ui eight wide column'>
+            {/* <img key={image} src={image} alt='Hog'/> 
+            <p key={name}> {name} </p>  */}
+            <div key = {id+image}  className="image">
+                <img onClick={()=>handleOnClick} key={image} src={image} alt='Hog'/>
+            </div>
+            <div key = {id+name} className="content">
+                {/* <a className="header">{name}</a> */}
+                <p className="header" key={name}> {name} </p>  
+            </div>
         </div> 
     )
 }
